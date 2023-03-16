7b19569fde6935cf367075113ed1e2fd5761b0e5
Yandr2022 <sharkievich@gmail.com>
2023-03-16 05:58:12 +0300

    added service level

--- src/main/java/com/yandr2022/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/yandr2022/spring/mvc_hibernate_aop/controller/MyController.java
@@ -1,7 +1,8 @@
 package com.yandr2022.spring.mvc_hibernate_aop.controller;
 
-import com.yandr2022.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+
 import com.yandr2022.spring.mvc_hibernate_aop.entity.Employee;
+import com.yandr2022.spring.mvc_hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +13,12 @@ import java.util.List;
 @Controller
 public class MyController {
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @RequestMapping("/")
     public String showAllEmployees(Model model) {
 
-        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmps", allEmployees);
 
         return "all-employees";
